5979462f6aef8158a625fb2fbb16d3a856bc0fe4
Rob Mason <workingmanrob@gmail.com>
2026-06-09 01:50:47 -0400

    make PIPE a var

--- game-start.sh
+++ game-start.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
+#
+#
+# if using flatpak dolphin this should be the standard path
+PIPE="~/.var/app/org.DolphinEmu.dolphin-emu/data/dolphin-emu/Pipes/gc_pipe"
+#
 # Not really needed now that dual input mode is working - just nice to have
 echo "This should be run when the white screen with CHUNsoft logo appears"
 echo
 sleep 2
 echo "Press A twice to get to your room"
-echo 'PRESS A' > Pipes/gc_pipe ; sleep .1 ; echo 'RELEASE A' > Pipes/gc_pipe
+echo 'PRESS A' > $PIPE ; sleep .1 ; echo 'RELEASE A' > $PIPE
 sleep 5
-echo 'PRESS A' > Pipes/gc_pipe ; sleep .1 ; echo 'RELEASE A' > Pipes/gc_pipe
+echo 'PRESS A' > $PIPE ; sleep .1 ; echo 'RELEASE A' > $PIPE
